a894294740feed182f2603c3264c50e3e027fe70
pulu64 <2093811998@qq.com>
2025-07-25 16:35:59 +0800

    build(app): 优化 APK 构建配置和性能
    - 配置 ndk 仅保留 64 位 ARM 架构，减少 APK 大小
    - 更新 ProGuard 规则，提高代码混淆和优化效果
    - 调整字体资源路径，确保正确打包
    - 新增输出元数据文件，优化构建过程

--- react-native.config.js
+++ react-native.config.js
@@ -3,5 +3,5 @@ module.exports = {
       ios: {},
       android: {},
     },
-    assets: ['./assets/fonts'],  // 指向你的字体文件夹路径
+    assets: ['./android/app/src/main/res/font'],  // 指向你的字体文件夹路径
   };
